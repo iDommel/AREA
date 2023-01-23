22332baa1824a7ead9292ff883c80609b5519f76
Iona Dommel-Prioux <iona.dommel-prioux@epitech.eu>
2023-01-23 15:46:59 +0100

    ✨ UPDATED: added about route

--- server/source/routes/mobile-apk.ts
+++ server/source/routes/mobile-apk.ts
@@ -3,7 +3,6 @@ import controller from '../controllers/mobile-apk';
 
 const router = express.Router();
 
-router.get('/download', controller.getApk);
-router.get('/', controller.test);
+router.get('/', controller.getApk);
 
 export = router;
