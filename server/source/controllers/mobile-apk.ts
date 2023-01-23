61c32ee52df7d52aa0341d23304a1a3528a7bce4
Iona Dommel-Prioux <iona.dommel-prioux@epitech.eu>
2023-01-23 13:25:58 +0100

    👷 UPDATED: docker-compose update to have a shared volume for the APK

---
+++ server/source/controllers/mobile-apk.ts
@@ -0,0 +1,23 @@
+import { NextFunction, Request, Response } from 'express';
+import fs from 'fs';
+
+const getApk = (req: Request, res: Response, next: NextFunction) => {
+    const apkPath: string | undefined = fs.readdirSync('/usr/src/app/apk').find((file) => file.endsWith('.apk'));
+    const finalPath = `/usr/src/app/apk/${apkPath}`;
+    if (!apkPath) {
+        return res.status(404).json({
+            message: 'No .apk file found in /usr/src/app/apk'
+        });
+    } else {
+        return res.status(200).download(finalPath, 'area.apk');
+    }
+};
+
+const test = (req: Request, res: Response, next: NextFunction) => {
+    console.log('test() called');
+    res.status(200).json({
+        message: 'test() called'
+    });
+};
+
+export default { getApk, test };
