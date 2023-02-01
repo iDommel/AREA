d76eaef478fe128fb014f8d459941c2ad3f79d81
Mfolio2004 <maxence.folio@epitech.eu>
2023-02-01 11:09:35 +0100

    EDITED: Form WorkflowPage

--- client-web/src/WorkflowPage.tsx
+++ client-web/src/WorkflowPage.tsx
@@ -1,43 +1,24 @@
-import React from "react";
 import { useState } from "react";
 import "./App.css";
 import User from "./User";
 
 type select = {
     type: string;
-    options: string[];
+    optionsService: string[];
+    optionsAction: string[];
 };
 
-const Select = ({ type, options }: select) => {
-  const [value, setValue] = useState("0");
-  const base = "Choisissez une " + type;
+const Select = ({ type, optionsService, optionsAction }: select) => {
+  const [action, setAction] = useState("0");
+  const [service, setService] = useState("0");
 
-  const handleChange = (event: any) => {
-    setValue(event.target.value);
+  const handleChangeService = (event: any) => {
+    setService(event.target.value);
   };
 
-  return (
-    <div className="select">
-      <select
-        value={value}
-        onChange={(event) => handleChange(event)}
-        className="SelectOption"
-      >
-        <option value="0">{base}</option>
-        <option value="1">{options[0]}</option>
-        <option value="2">{options[1]}</option>
-      </select>
-    </div>
-  );
-};
-
-type type = {
-    type: string;
-}; 
-
-const Area = ({ type }: type) => {
-  const [service] = useState("0");
-  const [action] = useState("0");
+  const handleChangeAction = (event: any) => {
+    setAction(event.target.value);
+  };
 
   const handleSubmit = (event: any) => {
     if (service === "0" || action === "0") {
@@ -49,31 +30,53 @@ const Area = ({ type }: type) => {
       return;
     }
     if (type === "action")
-      alert("Le service sélectionné est : " + service + " et la réaction sélectionnée est : " + action);
-    else
       alert("Le service sélectionné est : " + service + " et l'action sélectionnée est : " + action);
+    else
+      alert("Le service sélectionné est : " + service + " et la réaction sélectionnée est : " + action);
     event.preventDefault();
   };
 
   return (
-    <div className="action">
-      <form
-        onSubmit={(e) => handleSubmit(e)}
-        style={{ display: "flex", flexDirection: "column" }}
+    <form
+      onSubmit={(e) => handleSubmit(e)}
+      style={{ display: "flex", flexDirection: "column", alignItems: "flex-start", width: "100%", height: "100%" }}
+    >
+      <select
+        value={service}
+        onChange={(event) => handleChangeService(event)}
+        className="SelectOption"
       >
-        <label>
-          <Select
-            type="service"
-            options={["Service 1", "Service 2"]}
-          />
-          <Select type={type} options={["Action 1", "Action 2"]} />
-          <input
-            type="text"
-            className="ActionInput"
-            placeholder="Entrez une description"
-          />
-        </label>
-      </form>
+        <option value="0">Choisissez un service</option>
+        <option value="1">{optionsService[0]}</option>
+        <option value="2">{optionsService[1]}</option>
+      </select>
+      <select
+        value={action}
+        onChange={(event) => handleChangeAction(event)}
+        className="SelectOption"
+      >
+        <option value="0">Choisissez une action</option>
+        <option value="1">{optionsAction[0]}</option>
+        <option value="2">{optionsAction[1]}</option>
+      </select>
+      <input type="text" className="ActionInput" placeholder="Entrez une description"/>
+    </form>
+  );
+};
+
+type type = {
+    type: string;
+}; 
+
+const Area = ({ type }: type) => {
+  
+  return (
+    <div className="action">
+      <Select
+        type={type}
+        optionsService={["Service 1", "Service 2"]}
+        optionsAction={["Action 1", "Action 2"]}
+      />
     </div>
   );
 };
