d71f64a49e3e0b0dbdefd513e8e9d28f3d6e36e4
Iona Dommel-Prioux <iona.dommel-prioux@epitech.eu>
2023-02-07 07:16:48 +0100

    ✨ ADDED: demo workflow

--- server/source/routes/spotify.ts
+++ server/source/routes/spotify.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import controller from '../controllers/spotify';
 import passport from 'passport';
 import passportSpotify from 'passport-spotify';
-
+import User from '../models/user';
 const router = express.Router();
 const SpotifyStrategy = passportSpotify.Strategy;
 
@@ -13,11 +13,16 @@ passport.use(
             clientSecret: '734ebfb934c84261963f5794e5783c9f',
             callbackURL: 'http://localhost:8080/spotify/callback'
         },
-        (accessToken, refreshToken, expires_in, profile, done) => {
+        async (accessToken, refreshToken, expires_in, profile, done) => {
             console.log('accessToken', accessToken);
             console.log('refreshToken', refreshToken);
             console.log('expires_in', expires_in);
             console.log('profile', profile);
+
+            const user = await User.findById('63e1714e0670f95f5af133f7');
+
+            user.services = [{ name: 'spotify', accessToken, refreshToken }];
+            user.save();
             return done(null, {
                 accessToken,
                 refreshToken,
@@ -27,7 +32,7 @@ passport.use(
     )
 );
 
-router.get('/login', passport.authenticate('spotify', { scope: ['user-read-email', 'user-read-private'] }));
+router.get('/login', passport.authenticate('spotify', { scope: ['user-read-email', 'user-read-private', 'playlist-modify-private', 'playlist-modify-public'] }));
 router.get('/callback', passport.authenticate('spotify', { failureRedirect: '/login' }), async (req, res) => {
     res.redirect('http://localhost:3000/Home');
 });
--- server/source/utils/cron.ts
+++ server/source/utils/cron.ts
@@ -1,10 +1,58 @@
 import CronJob from 'node-cron';
+import controller from '../controllers/time';
+import Workflow from '../models/workflow';
+import User from '../models/user';
+import SpotifyWebApi from 'spotify-web-api-node';
+
+type ActionType = {
+    _id: string;
+    name: string;
+    description: string;
+};
+
+let spotifyApi = new SpotifyWebApi({
+    clientId: 'd21affede3984ecea64c0ebaceff41e3',
+    clientSecret: '734ebfb934c84261963f5794e5783c9f',
+    redirectUri: 'http://localhost:3000/Home'
+});
+
+const spotifyReaction = async (newName: string) => {
+    try {
+        const user = await User.findById('63e1714e0670f95f5af133f7');
+
+        spotifyApi.setAccessToken(user.services[0].accessToken);
+        const res = await spotifyApi.changePlaylistDetails('0y0zkkH8WQCCKSXbG39dOa', {
+            name: newName,
+            public: false
+        });
+        console.log('Playlist is now private!');
+    } catch (error: any) {
+        console.log('Something went wrong!', error);
+    }
+};
+
+const checkActions = async () => {
+    try {
+        const workflows = await Workflow.find({}).sort({ createdAt: -1 }).limit(1).populate('actions');
+        //TODO: talk to Lucas about these any types
+        workflows.forEach((workflow: any) => {
+            workflow.actions.forEach(async (action: any) => {
+                if (action.name === 'isMinuteEven') {
+                    const isEven = await controller.isMinuteEven('Europe/Amsterdam');
+                    if (isEven) {
+                        console.log('Is minute even?', isEven);
+                        spotifyReaction(workflow.description);
+                    }
+                }
+            });
+        });
+    } catch (error: any) {
+        console.log(error);
+    }
+};
 
 const initScheduledJobs = () => {
-    const scheduledJobFunction = CronJob.schedule('*/5 * * * *', () => {
-        console.log("I'm executed on a schedule!");
-        // Add your custom logic here
-    });
+    const scheduledJobFunction = CronJob.schedule('* * * * *', checkActions);
 
     scheduledJobFunction.start();
 };
