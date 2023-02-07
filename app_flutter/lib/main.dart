613368fce66fd515f0f91ed35aac4c2884f45172
Mateo Baldassi <mateo.baldassi@epitech.eu>
2023-02-07 11:30:19 +0100

    [ADD] Plus button to flutter

--- app_flutter/lib/main.dart
+++ app_flutter/lib/main.dart
@@ -1,65 +1,84 @@
+import 'package:area_app/screens/home_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/basic.dart';
-import 'package:flutter/src/widgets/framework.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'AREA',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: MyHomePage(),
+      theme: ThemeData(primarySwatch: Colors.grey),
+      debugShowCheckedModeBanner: false,
+      home: const HomeScreen(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    final ButtonStyle style = TextButton.styleFrom( foregroundColor: Colors.black,
-      textStyle: const TextStyle(fontSize: 40),
-      backgroundColor: const Color.fromARGB(255, 217, 217, 217),
-    );
-    return Scaffold(
-      backgroundColor: Color.fromARGB(255, 101, 101, 101),
-      appBar: AppBar(
-        title: Text("AREA"),
-        centerTitle: true,
-        backgroundColor: Color.fromARGB(255, 73, 71, 131),
-      ),
-      body: Padding(
-        padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0),
-        child: Row(
-          children: <Widget>[
-            TextButton(
-              style: style,
-              onPressed: () {},
-              child: Text('+'),
-            ),
-          ],
-        ),
-        
-      ),
-    );
-  }
-}
-
-// class Workflow extends StatelessWidget {
+// class MyHomePage extends StatelessWidget {
 //   @override
 //   Widget build(BuildContext context) {
+//     final ButtonStyle style = TextButton.styleFrom(
+//       foregroundColor: Colors.black,
+//       textStyle: const TextStyle(fontSize: 45),
+//       backgroundColor: const Color.fromARGB(255, 217, 217, 217),
+//     );
 //     return Scaffold(
-//       floatingActionButton: FloatingActionButton(
-//         onPressed: () {},
-//         child: Text('+'),
+//       backgroundColor: Color.fromARGB(255, 101, 101, 101),
+//       appBar: AppBar(
+//         title: Text("AREA"),
+//         centerTitle: true,
+//         backgroundColor: Color.fromARGB(255, 73, 71, 131),
+//       ),
+//       body: Padding(
+//         padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0),
+//         child: Row(
+//           children: <Widget>[
+//             ButtonBar(
+//               mainAxisSize: MainAxisSize.min,
+//               children: <Widget>[
+//                 TextButton(
+//                   onPressed: () {},
+//                   style: style,
+//                   child: Text("+"),
+//                 ),
+//                 TextButton(
+//                   onPressed: () {},
+//                   style: style,
+//                   child: Text("+"),
+//                 ),
+//                 TextButton(
+//                   onPressed: () {},
+//                   style: style,
+//                   child: Text("+"),
+//                 ),
+//               ],
+//             ),
+//           ],
+//         ),
 //       ),
 //     );
 //   }
 // }
 
-// class Services extends StatelessWidget {
+// class WorkflowState extends StatelessWidget {
 //   @override
+//   Widget build(BuildContext context) {
+//     return ListView.builder(
+//       itemBuilder: (context, index) {
+//         return ListTile(),
+     
+//       },
+//     );
+//   }
+// }
+
+// Widget Services() {
+
 //   Widget build(BuildContext context) {
 //     return Scaffold(
 //       floatingActionButton: FloatingActionButton(
