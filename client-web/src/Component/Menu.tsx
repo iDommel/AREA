407678aebcd8c26cff3bb8230a2553f29597c31b
Mfolio2004 <maxence.folio@epitech.eu>
2023-02-15 10:55:05 +0100

    ADDED: workflow and service from API in menu

--- client-web/src/Component/Menu.tsx
+++ client-web/src/Component/Menu.tsx
@@ -23,25 +23,70 @@ function getItem(
   } as MenuItem;
 }
 
-const items: MenuItem[] = [
-  getItem('Home', '1' ,<HomeOutlined /> ),
-  getItem('Worklows', 'sub1', <AppstoreOutlined />, [
-    getItem('Create Workflow', '2'),
-    getItem('Workflow 1', '3'),
-    getItem('Workflow 2', '4'),
-  ]),
-  getItem('Services', 'sub2', <SettingOutlined />, [
-    getItem('Spotify', '5'),
-    getItem('Timer', '6'),
-  ]),
-];
-
-// submenu keys of first level
 const rootSubmenuKeys = ['sub1', 'sub2'];
 
 const MenuPage: React.FC = () => {
   const [openKeys, setOpenKeys] = useState(['sub1']);
   const navigate = useNavigate();
+  const [items, setItems] = useState<MenuItem[]>([
+    getItem('Home', '1' ,<HomeOutlined /> ),
+    getItem('Workflows', '2', <AppstoreOutlined />)
+  ]);
+
+  const getWorkflows = async () => {
+    try {
+      const response = await fetch("http://localhost:8080/workflows", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const data = await response.json();
+      if (response.status !== 200 || !data.workflows) {
+        alert(data.message);
+      } else {
+        const workflows = data.workflows;
+        const workflowItems: MenuItem[] = [];
+        workflows.map((workflow : any) => {
+          console.log(workflow._id, workflow.name );
+          workflowItems.push(getItem(workflow.name, workflow._id))
+        });
+        items[1] = getItem('Workflows', '2', <AppstoreOutlined />, workflowItems);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const getServices = async () => {
+    try {
+      const response = await fetch("http://localhost:8080/services", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const data = await response.json();
+      if (response.status !== 200 || !data.services) {
+        alert(data.message);
+      } else {
+        const services = data.services;
+        const serviceItems: MenuItem[] = [];
+        services.map((service : any) => {
+          console.log(service._id, service.name );
+          serviceItems.push(getItem(service.name, service._id))
+        });
+        setItems([...items, getItem('Services', '3', <SettingOutlined />, serviceItems)]);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  React.useEffect(() => {
+    getWorkflows();
+    getServices();
+  }, []);
 
   const onOpenChange: MenuProps['onOpenChange'] = (keys) => {
     const latestOpenKey = keys.find((key) => openKeys.indexOf(key) === -1);
@@ -55,7 +100,7 @@ const MenuPage: React.FC = () => {
   const handleSelect = (e: any) => {
     if (e.key === '1') {
         navigate('/home');
-    } else if (e.key === '2') {
+    } else if (e.key === '2-1') {
         navigate('/Workflow');
     }
   };
