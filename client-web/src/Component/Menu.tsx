b3d39403704c40096f8c558ffd0a1366059f3bc7
iDommel <61154896+iDommel@users.noreply.github.com>
2023-02-20 16:14:31 +0100

    🎨 🐛 FIXED: linter and navigation issue

--- client-web/src/Component/Menu.tsx
+++ client-web/src/Component/Menu.tsx
@@ -1,17 +1,21 @@
-import React, { useState } from 'react';
-import { AppstoreOutlined, HomeOutlined, SettingOutlined } from '@ant-design/icons';
-import type { MenuProps } from 'antd';
-import { Menu, message } from 'antd';
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import {
+  AppstoreOutlined,
+  HomeOutlined,
+  SettingOutlined,
+} from "@ant-design/icons";
+import type { MenuProps } from "antd";
+import { Menu, message } from "antd";
+import { useNavigate } from "react-router-dom";
 
-type MenuItem = Required<MenuProps>['items'][number];
+type MenuItem = Required<MenuProps>["items"][number];
 
 function getItem(
   label: React.ReactNode,
   key: React.Key,
   icon?: React.ReactNode,
   children?: MenuItem[],
-  type?: 'group',
+  type?: "group"
 ): MenuItem {
   return {
     key,
@@ -24,14 +28,14 @@ function getItem(
 
 const serviceList = ["63e1a99689b9860fb46d3698", "63e1ab6fe3a0d2130314394b"];
 
-const rootSubmenuKeys = ['sub1', 'sub2'];
+const rootSubmenuKeys = ["sub1", "sub2"];
 
 const MenuPage: React.FC = () => {
-  const [openKeys, setOpenKeys] = useState(['sub1']);
+  const [openKeys, setOpenKeys] = useState(["sub1"]);
   const navigate = useNavigate();
   const [items, setItems] = useState<MenuItem[]>([
-    getItem('Home', '1' ,<HomeOutlined /> ),
-    getItem('Workflows', '2', <AppstoreOutlined />)
+    getItem("Home", "1", <HomeOutlined />),
+    getItem("Workflows", "2", <AppstoreOutlined />),
   ]);
 
   const getWorkflows = async () => {
@@ -48,11 +52,16 @@ const MenuPage: React.FC = () => {
       } else {
         const workflows = data.workflows;
         const workflowItems: MenuItem[] = [];
-        workflows.map((workflow : any) => {
-          console.log(workflow._id, workflow.name );
-          workflowItems.push(getItem(workflow.name, workflow._id))
+        workflows.map((workflow: any) => {
+          console.log(workflow._id, workflow.name);
+          workflowItems.push(getItem(workflow.name, workflow._id));
         });
-        items[1] = getItem('Workflows', '2', <AppstoreOutlined />, workflowItems);
+        items[1] = getItem(
+          "Workflows",
+          "2",
+          <AppstoreOutlined />,
+          workflowItems
+        );
       }
     } catch (error) {
       console.error(error);
@@ -73,11 +82,14 @@ const MenuPage: React.FC = () => {
       } else {
         const services = data.services;
         const serviceItems: MenuItem[] = [];
-        services.map((service : any) => {
-          console.log(service._id, service.name );
-          serviceItems.push(getItem(service.name, service._id))
+        services.map((service: any) => {
+          console.log(service._id, service.name);
+          serviceItems.push(getItem(service.name, service._id));
         });
-        setItems([...items, getItem('Services', '3', <SettingOutlined />, serviceItems)]);
+        setItems([
+          ...items,
+          getItem("Services", "3", <SettingOutlined />, serviceItems),
+        ]);
       }
     } catch (error) {
       console.error(error);
@@ -89,7 +101,7 @@ const MenuPage: React.FC = () => {
     getServices();
   }, []);
 
-  const onOpenChange: MenuProps['onOpenChange'] = (keys) => {
+  const onOpenChange: MenuProps["onOpenChange"] = (keys) => {
     const latestOpenKey = keys.find((key) => openKeys.indexOf(key) === -1);
     if (rootSubmenuKeys.indexOf(latestOpenKey!) === -1) {
       setOpenKeys(keys);
@@ -100,14 +112,14 @@ const MenuPage: React.FC = () => {
 
   const handleSelect = (e: any) => {
     console.log(e);
-    if (e.key === '1') {
-        navigate('/home');
-    } else if (e.key === '2-1') {
-        navigate('/Workflow');
+    if (e.key === "1") {
+      navigate("/home");
+    } else if (e.key === "2-1") {
+      navigate("/Workflow");
     } else if (serviceList.includes(e.key)) {
-        message.info('Service n°' + e.key + ' is not implemented yet');
+      message.info("Service n°" + e.key + " is not implemented yet");
     } else {
-        navigate('/WorkflowInfo/id=' + e.key);
+      navigate("/WorkflowInfo/" + e.key);
     }
   };
 
@@ -123,6 +135,5 @@ const MenuPage: React.FC = () => {
     />
   );
 };
-
 
 export default MenuPage;
