ef280855fa1fb555634f15bb8a829be07a4d3b59
iDommel <61154896+iDommel@users.noreply.github.com>
2023-02-21 03:52:27 +0100

    🐛 UPDATED: navigation and register page

--- client-web/src/index.tsx
+++ client-web/src/index.tsx
@@ -1,17 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
 
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 
 import Login from "./Login";
 import Home from "./App";
 import WorkflowPage from "./WorkflowPage";
-import WorkflowInfo from './WorkflowInfo';
+import WorkflowInfo from "./WorkflowInfo";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <div style={{ width: "100%", height: "100vh" }}>
@@ -19,6 +19,8 @@ root.render(
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Login />} />
+          <Route path="/Login" element={<Login />} />
+          <Route path="/Register" element={<Login isLoginMode={false} />} />
           <Route path="/Home" element={<Home />} />
           <Route path="/Workflow" element={<WorkflowPage />} />
           <Route path="/WorkflowInfo/:id" element={<WorkflowInfo />} />
