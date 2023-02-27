f946156966b73e97c3a83686fc0a5c992d2e4b11
apenot <antoine.penot@epitech.eu>
2023-02-27 15:47:58 +0100

    MERGED: dev

--- server/source/routes/gitlab.ts
+++ server/source/routes/gitlab.ts
@@ -10,14 +10,14 @@ const GitLabStrategy = passportGitLab.Strategy;
 const clientID = process.env.GITLAB_APP_ID as string;
 const clientSecret = process.env.GITLAB_APP_SECRET as string;
 
-passport.use(new GitLabStrategy ({
-    clientID,
-    clientSecret,
-    callbackURL: "http://localhost:3000/gitlab/callback"
-  },
-    async (accessToken: string, refreshToken: string, profile: passportGitLab.Profile, done: (err: any, user?: any) => void) => {
-    User.findOrCreate({gitlabId: profile.id}, function (err, user) {
-      return cb(err, user);
-    });
-  }
-));
+// passport.use(new GitLabStrategy ({
+//     clientID,
+//     clientSecret,
+//     callbackURL: "http://localhost:3000/gitlab/callback"
+//   },
+//     async (accessToken: string, refreshToken: string, profile: any, done: (err: any, user?: any) => void) => {
+//     User.findOrCreate({gitlabId: profile.id}, function (err, user) {
+//       return cb(err, user);
+//     });
+//   }
+// ));
