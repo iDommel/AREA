6cc9c3a3bbf28e49c2c1a71c6873bbe8b61d3573
Iona Dommel <iona.dommel-prioux@epitech.eu>
2023-02-28 11:04:23 +0100

    ♻️ UPDATED: refactored frontend to integrate an authcontext

--- client-web/src/index.tsx
+++ client-web/src/index.tsx
@@ -1,34 +1,13 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 
-import { Routes, Route, BrowserRouter } from "react-router-dom";
-
-import Login from "./Login";
-import Home from "./App";
-import WorkflowPage from "./WorkflowPage";
-import WorkflowInfo from "./WorkflowInfo";
+import App from "./App";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
-root.render(
-  <div style={{ width: "100%", height: "100vh" }}>
-    <React.StrictMode>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/Login" element={<Login />} />
-          <Route path="/Register" element={<Login isLoginMode={false} />} />
-          <Route path="/Home/" element={<Home />} />
-          <Route path="/Workflow" element={<WorkflowPage />} />
-          <Route path="/WorkflowInfo/:id" element={<WorkflowInfo />} />
-        </Routes>
-      </BrowserRouter>
-    </React.StrictMode>
-  </div>
-);
+root.render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
