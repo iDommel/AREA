ce1f0c8c022dc5f2c6cab8055cdcfea1a0375b6c
mateo.baldassi <mateo.baldassi@epitech.eu>
2023-02-28 14:44:51 +0100

    [ADD] All route in front

--- app_flutter/lib/main.dart
+++ app_flutter/lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:area_app/screens/add_reaction_screen.dart';
+import 'package:area_app/screens/create_workflow_screen.dart';
 import 'package:area_app/screens/home_screen.dart';
 import 'package:area_app/screens/login_screen.dart';
 import 'package:flutter/material.dart';
@@ -12,13 +14,19 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      initialRoute: '/',
+      routes: {
+        '/': (context) => LoginScreen(),
+        '/homePage': (context) => HomeScreen(),
+        '/homePage/createWorkflow': (context) => CreateWorkflowScreen(),
+        '/homePage/createWorkflow/addReaction': (context) => AddReactionView()
+      },
       title: 'AREA',
       theme: ThemeData(
           primaryColor: Color.fromARGB(255, 67, 67, 67),
           backgroundColor: Color.fromARGB(255, 67, 67, 67),
           scaffoldBackgroundColor: Color.fromARGB(255, 101, 101, 101)),
       debugShowCheckedModeBanner: false,
-      home: const LoginScreen(),
     );
   }
 }
