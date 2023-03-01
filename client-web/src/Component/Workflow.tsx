97877a30266dc43a3817d956624bb21618b1601a
Iona Dommel <iona.dommel-prioux@epitech.eu>
2023-03-01 10:34:58 +0100

    🐛 FIXED: image display

--- client-web/src/Component/Workflow.tsx
+++ client-web/src/Component/Workflow.tsx
@@ -10,7 +10,14 @@ type AppProps = {
   service2: string;
 };
 
-const Workflow = ({ id, name, isActivated, preview, service1, service2 }: AppProps) => {
+const Workflow = ({
+  id,
+  name,
+  isActivated,
+  preview,
+  service1,
+  service2,
+}: AppProps) => {
   const navigate = useNavigate();
 
   const goToWorkflow = (id: string) => {
@@ -21,13 +28,47 @@ const Workflow = ({ id, name, isActivated, preview, service1, service2 }: AppPro
     }
   };
 
+  const renderServiceIcons = () => {
+    if (service1 && service2) {
+      return (
+        <div className="serviceIcons">
+          <img
+            src={
+              "https://img.icons8.com/ios/40/000000/" +
+              service1.toLowerCase() +
+              ".png"
+            }
+            alt={service1}
+          />
+          <img
+            src={"https://img.icons8.com/ios/30/000000/arrow.png"}
+            alt="arrow"
+          />
+          <img
+            src={
+              "https://img.icons8.com/ios/40/000000/" +
+              service2.toLowerCase() +
+              ".png"
+            }
+            alt={service2}
+          />
+        </div>
+      );
+    } else {
+      return <div className="serviceIcons">No workflows</div>;
+    }
+  };
+
   return (
     <div className="workflow">
-      <Card className="button" title={name} onClick={() => goToWorkflow(id)} headStyle={{ borderColor : "#000000" }}>
+      <Card
+        className="button"
+        title={name}
+        onClick={() => goToWorkflow(id)}
+        headStyle={{ borderColor: "#000000" }}
+      >
         {preview}
-        <img src={"https://img.icons8.com/ios/40/000000/" + service1.toLowerCase() + ".png"} alt={service1} />
-        <img src={"https://img.icons8.com/ios/30/000000/arrow.png"} alt="arrow" />
-        <img src={"https://img.icons8.com/ios/40/000000/" + service2.toLowerCase() + ".png"} alt={service2} />
+        {renderServiceIcons()}
       </Card>
     </div>
   );
