32143b3b92bce1783a63c07104d2f5db88b671c5
Iona Dommel <iona.dommel-prioux@epitech.eu>
2023-03-04 15:12:28 +0100

    DELEDED: app_flutter

---
+++ mobile/windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\ionan\\src\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\ionan\\dev\\AREA\\mobile" PROJECT_DIR)
+
+set(FLUTTER_VERSION "0.0.1+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\ionan\\src\\flutter"
+  "PROJECT_DIR=C:\\Users\\ionan\\dev\\AREA\\mobile"
+  "FLUTTER_ROOT=C:\\Users\\ionan\\src\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\ionan\\dev\\AREA\\mobile\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\ionan\\dev\\AREA\\mobile"
+  "FLUTTER_TARGET=C:\\Users\\ionan\\dev\\AREA\\mobile\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\ionan\\dev\\AREA\\mobile\\.dart_tool\\package_config.json"
+)
