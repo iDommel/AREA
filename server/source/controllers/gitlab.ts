77c7765eda98c6bd1dc7887b3195d943797f4225
apenot <antoine.penot@epitech.eu>
2023-03-04 15:56:42 +0100

    🚧 💩 ADDED: beginning of gitlab reaction

--- server/source/controllers/gitlab.ts
+++ server/source/controllers/gitlab.ts
@@ -2,7 +2,92 @@ import { Request, Response } from 'express';
 import ServiceStatus from '../models/serviceStatus';
 import { getUserIdFromCookie } from '../utils/utils';
 import Service from '../models/service';
+import axios, { AxiosRequestConfig } from "axios";
 
+const create_commit = async (req: Request, res: Response) => {
+    try {
+        const projectId = req.body.projectId;
+        const access_token = req.headers.authorization?.split(" ")[1];
+        const branch = req.body.branch;
+        const commit_message = req.body.message;
+        const file_path = req.body.file_path;
+        const content = req.body.content;
+
+        const axiosConfig: AxiosRequestConfig = {
+            headers: {
+                Authorization: `Bearer ${access_token}`,
+            },
+        };
+
+        // Get latest commit sha of the branch
+        const { data: latestCommit } = await axios.get(
+            `https://gitlab.com/api/v4/projects/${projectId}/repository/branches/${branch}`,
+            axiosConfig
+        );
+
+        const commitPayload = {
+            branch,
+            commit_message,
+            actions: [
+                {
+                    action: "create",
+                    file_path,
+                    content,
+                },
+            ],
+            author_email: "example@example.com",
+            author_name: "Example User",
+            start_branch: branch,
+            start_sha: latestCommit.commit.id,
+        };
+
+        const { data: new_commit } = await axios.post(
+            `https://gitlab.com/api/v4/projects/${projectId}/repository/commits`,
+            commitPayload,
+            axiosConfig
+        );
+
+        res.status(200).json(new_commit);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error });
+    }
+};
+
+const gitlabCommit = async (workflow: Workflow): Promise<GitlabCommitResponse> => {
+    try {
+        const info = workflow.additionalData[0];
+        const gitlab = await ServiceStatus.findOne({ serviceName: 'GitLab', user: workflow.relativeUser });
+
+        const response = await axios.post(`https://gitlab.com/api/v4/projects/${info.projectId}/repository/commits`, {
+            branch: info.branch,
+            commit_message: info.commitMessage,
+            actions: [
+                {
+                    action: 'create',
+                    file_path: info.filePath,
+                    content: info.fileContent
+                }
+            ]
+        }, {
+            headers: {
+                'Content-Type': 'application/json',
+                'PRIVATE-TOKEN': gitlab.auth.accessToken
+            }
+        });
+
+        if (response.status !== 201) {
+            console.log(`Something went wrong with GitLab commit! ${response.data.message}`);
+            return { success: false, message: response.data.message };
+        } else {
+            console.log('GitLab commit created!');
+            return { success: true };
+        }
+    } catch (error: any) {
+        console.log('Something went wrong with GitLab commit!', error);
+        return { success: false, message: error.message };
+    }
+};
 
 const logout = async (req: Request, res: Response) => {
     try {
